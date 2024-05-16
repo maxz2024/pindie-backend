20042ab4c1970f013afcb42a9e071dd73494359b
maxz2024 <MaksimZdonov456@yandex.ru>
2024-05-16 14:42:25 +0300

    add  frontend

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const authRouter = require("express").Router();
 const { login } = require("../controllers/auth.js");
 
-authRouter.post("/login", login);
+authRouter.post("/auth/login", login);
+
 
 module.exports = authRouter;
 
