9f586f7ca8d2747e9eca226baaff902751e0e004
maxz2024 <MaksimZdonov456@yandex.ru>
2024-05-19 21:55:00 +0300

    Печеньки программистов Зона для админов

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const connectToDatabase = require("./database/connect");
 const cors = require("./middlewares/cors");
 const apiRouter = require("./routes/apiRouter");
+const cookieParser = require("cookie-parser");
+const pagesRouter = require("./routes/pages");
 
 const app = express();
 const PORT = 3000;
@@ -12,7 +14,9 @@ connectToDatabase();
 
 app.use(
   cors,
+  cookieParser(),
   bodyParser.json(),
+  pagesRouter,
   apiRouter,
   express.static(path.join(__dirname, "public")),
 );
